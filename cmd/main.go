daab98717f0ee179083ffddb5f9dc835ab33f57c
hxuan190 <hxuan190@gmail.com>
2025-03-16 20:21:06 +0700

    update .gitignore to include .vscode; refactor main.go for improved logging and graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -30,9 +30,6 @@ func main() {
 		port = "8080"
 	}
 
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
-
 	r := gin.Default()
 	docs.SwaggerInfo.BasePath = ""
 	r.Use(gin.Recovery())
@@ -42,12 +39,12 @@ func main() {
 	config.AllowCredentials = true
 	config.AddAllowHeaders("Authorization")
 	r.Use(cors.New(config))
-
-	// Validation endpoints
-	r.GET("/ping", ping)
+	
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-
+	// Validation endpoints
 	r.Use(middleware.Logging())
+	r.GET("/ping", ping)
+
 	// v1 := r.Group("/v1")
 	srv := &http.Server{
 		Addr:    ":8080",
@@ -57,12 +54,11 @@ func main() {
 	go func() {
 		// service connections
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logrus.WithError(err).Error("listen: %s\n")
+			logrus.WithError(err).Error("listen error")
 		}
 	}()
 
 	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
 	quit := make(chan os.Signal, 1)
 	// kill (no param) default send syscall.SIGTERM
 	// kill -2 is syscall.SIGINT
@@ -71,12 +67,11 @@ func main() {
 	<-quit
 	log.Println("Shutdown Server ...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		logrus.WithError(err).Error("Server Shutdown: %s\n")
+		logrus.WithError(err).Error("Server Shutdown Failed")
 	}
-	// catching ctx.Done(). timeout of 5 seconds.
 	select {
 	case <-ctx.Done():
 		logrus.Info("timeout of 5 seconds.")
