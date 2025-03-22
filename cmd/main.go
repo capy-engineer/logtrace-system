b8741c2d405581624bce696db90fdf129020711d
hxuan190 <hxuan190@gmail.com>
2025-03-22 22:08:06 +0700

    setup docker compose for NATS

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/nats-io/nats.go"
-	"github.com/nats-io/nats.go/jetstream"
 	"log"
 	"logtrace/docs"
 	"logtrace/middleware"
@@ -13,6 +11,9 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -33,22 +34,17 @@ func main() {
 		port = "8080"
 	}
 
-	// connect to nats server
 	nc, _ := nats.Connect(nats.DefaultURL)
 
-	// create jetstream context from nats connection
 	js, _ := jetstream.New(nc)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	// get existing stream handle
 	stream, _ := js.Stream(ctx, "foo")
 
-	// retrieve consumer handle from a stream
 	cons, _ := stream.Consumer(ctx, "cons")
 
-	// consume messages from the consumer in callback
 	cc, _ := cons.Consume(func(msg jetstream.Msg) {
 		fmt.Println("Received jetstream message: ", string(msg.Data()))
 		msg.Ack()
